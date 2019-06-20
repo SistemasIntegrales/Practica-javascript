1577d8967fbba0a25687e5b8c4aebd7bb9103804
Emiliano Cervantes <emiliano@iMac-de-Emiliano.local>
2019-06-20 12:13:47 -0500

    Map listo

--- map.js
+++ map.js
@@ -4,6 +4,11 @@
 * EN UNA LISTA DE INFORMACIÓN
 */
 
+/*
+* GENERALMENTE ITERACIONES DE LISTAS DE DATOS
+* EJ: INFO DE UN MURO (POSTS)
+*/
+
 const numeros = [1,2,3];
 let dobleNumeros = [];
 
@@ -12,6 +17,7 @@ let dobleNumeros = [];
 dobleNumeros = numeros.map(function(numero) {
   return numero * 2;
 });
+
 
 //Ver el listado de precios que existen
 let cars = [
@@ -25,3 +31,44 @@ let precios = cars.map(function(car) {
     return car.precio;
   }
 });
+
+
+// SACAR LA ALTURA Y AREA DE UNA IMAGEN
+var images = [
+  { height: '34px', width: '39px' },
+  { height: '54px', width: '19px' },
+  { height: '83px', width: '75px' },
+];
+
+var heights = images.map(function(image) {
+  return image.height;
+});
+var area = images.map(function(image) {
+  return (parseInt(image.height, 10)  * parseInt(image.width, 10));
+});
+
+
+//CALCULAR LA VELOCIDAD Y ALMACENARLO EN UNA VARIABLE
+var trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 }
+];
+var speeds = trips.map(function(trip) {
+  return trip.distance / trip.time;
+});
+
+
+//FUNCION QUE REGRESE UN ARREGLO DE LAS PROPIEDADES SOLICITADAS
+//Ejemplo de arreglo:
+var paints = [{color: 'rojo', size:'10'}, {color: 'azul', size:'15'}];
+
+function pluck(array, property) {
+  return array.map(function(element) {
+    return element[property];
+  });
+}
+var colors = pluck(paints, 'color');
+
+/* SOLUCIÓN ALTERNATIVA CON FUNCIONES FLECHA */
+const pluck = (array, property) => array.map(arr => arr[property]);
