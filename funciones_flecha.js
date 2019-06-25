43097a2f7a8dad13bc20ac8e562fd7c10cd7fe80
Emiliano Cervantes <emiliano@iMac-de-Emiliano.local>
2019-06-25 13:52:26 -0500

    Funciones flecha

--- funciones_flecha.js
+++ funciones_flecha.js
@@ -54,3 +54,91 @@ numeros.map(function(numero) {
 
 // Refactorización
 numeros.map(numero => numero * 2);
+
+
+
+const equipo = {
+  miembros: ['Jane', 'Bill'],
+  nombreEquipo: 'Super S',
+  resumen: function() {
+    return this.miembros.map(function(miembro) {
+      return `${miembro} está en el equipo ${this.nombreEquipo}`; // this.nombreEquipo está como no definido
+    });
+  }
+};
+// Aunque está la función adentro, no se reconoce como si fuera suya
+// La función correo por sí sola, como si no fuera parte de 'equipo'
+
+// DOS FORMAS de resolverlo:
+// 1) Con ES5 (a la antiguita)
+const equipo = {
+  miembros: ['Jane', 'Bill'],
+  nombreEquipo: 'Super S',
+  resumen: function() {
+    return this.miembros.map(function(miembro) {
+      return `${miembro} está en el equipo ${this.nombreEquipo}`; // this.nombreEquipo está como no definido
+    }.bind(this)); // Así se liga la función. Se une el contexto de la función al contexto actual.
+  }
+};
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Function/bind
+const equipo = {
+  miembros: ['Jane', 'Bill'],
+  nombreEquipo: 'Super S',
+  resumen: function() {
+    return this.miembros.map(function(miembro) {
+      return `${miembro} está en el equipo ${equipo.nombreEquipo}`; // Cambiando this por equipo pero en una función no es práctico
+    });
+  }
+};
+
+
+// 2) Con ES6, FUNCIONES FLECHA
+const equipo = {
+  miembros: ['Jane', 'Bill'],
+  nombreEquipo: 'Super S',
+  resumen: function() {
+    // Aquí this === equipo
+    return this.miembros.map(miembro => `${miembro} está en el equipo ${this.nombreEquipo}`);
+  } // Funciones flecha hacen uso de LEXICAL THIS
+}; // 'This' se refiere al contexto original
+
+// https://hackernoon.com/javascript-es6-arrow-functions-and-lexical-this-f2a3e2a5e8c4
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Funciones/Arrow_functions
+
+equipo.resumen();
+
+
+/*
+* EJERCICIOS
+*
+*/
+
+// Refactorizar
+const fibonacci = function(n) {
+  if (n < 3) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+const fibonacci = n => {
+  if (n < 3) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+// Agregar llave getName para regresar this.name
+// NOTA: funciones flecha NO son siempre la solución
+// Dos posibles y cortas:
+const profile = {
+  name: 'Alex',
+  getName: function() {
+    return this.name;
+  }
+};
+const profile = {
+  name: 'Alex',
+  getName() {
+    return this.name;
+  }
+};
+
+profile.getName();
+// https://derickbailey.com/2015/09/28/do-es6-arrow-functions-really-solve-this-in-javascript/
